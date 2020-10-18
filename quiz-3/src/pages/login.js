c402e1f347ae699c44cf82f7713408b3845586fa
f1reboywatergrl <vincentius.samuelgk2@gmail.com>
2020-10-18 21:47:58 +0700

    Update login.js

--- quiz-3/src/pages/login.js
+++ quiz-3/src/pages/login.js
@@ -3,28 +3,24 @@ import '../tugas-2/Tugas-2/public/css/style.css';
 import {Context} from "./main.js"
 
 const Login = (props) => {
-
-    /*const [login, setLogin] = useContext(Context)
-    const [form, setForm] = useState(-1);*/
-
     
     return (
         <div className="App">
             <section>
                 <div style={{display:"flex", justifyContent: "center", alignItems: "center"}}>
-                    <h3>Username : </h3>
-                    <input type="text" style={{marginLeft: "1%"}} />
+                    <h3 style={{margin:"5px"}}>Enter username : </h3>
+                    <input type="text" style={{margin: "10px"}} />
                 </div>
                 <div style={{display:"flex", justifyContent: "center", alignItems: "center"}}>
-                    <h3>Password : </h3>
-                    <input type="text" style={{marginLeft: "1%"}}/>
+                    <h3 style={{margin:"5px"}}>Password : </h3>
+                    <input type="text" style={{margin: "10px"}}/>
                 </div>
                 <div style={{display:"flex", justifyContent: "center", alignItems: "center"}}>
-                    <button>Login</button>
+                    <button style={{margin:"5px"}}>Login</button>
                 </div>
             </section>
         </div>
     );
-  }//onClick={() => setLogin(true)}
+  }
 
   export default Login
